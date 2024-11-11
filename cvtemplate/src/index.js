934eb1e7894f4a2eb5f1f0b68a97b013f4ae9828
ZsoltSzilagyi1 <z_szilgyi@yahoo.com>
2024-11-11 13:48:07 +0200

    recap curs1

---
+++ cvtemplate/src/index.js
@@ -0,0 +1,8 @@
+// Import our custom css
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+
+import particles from 'particles.js'
+import './js/particles.js'
+
+import './js/main.js'
